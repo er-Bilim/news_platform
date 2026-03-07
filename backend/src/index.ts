712409aaa10fac9db7b627758729d390f922a417
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-03-07 14:54:34 +0600

    feat(backend): implement GET and POST routes for news module

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,20 @@
-import express from "express";
-import type { Express } from "express";
-import cors from "cors";
-import { PORT } from "./constants/constants";
-import apiRoute from "./routes/api.route";
+import express from 'express';
+import type { Express } from 'express';
+import cors from 'cors';
+import { PORT } from './constants/constants';
+import apiRoute from './routes/api.route';
+import mysqlDb from './config/mysqlDb';
+import dotenv from 'dotenv';
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/", apiRoute);
+dotenv.config();
+app.use('/api/', apiRoute);
 
 const run = async () => {
+  await mysqlDb.init();
   app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
   });
--- backend/src/routes/news/news.route.ts
+++ backend/src/routes/news/news.route.ts
@@ -1,10 +1,67 @@
-import type { Request, Response, Router } from "express";
-import express from "express";
+import type { Request, Response, Router } from 'express';
+import express from 'express';
+import { imagesUpload } from '../../middlewares/multer';
+import { INews, INewsWithoutContent } from '../../types/news/news.types';
+import type { Connection, ResultSetHeader } from 'mysql2/promise';
+import mysqlDb from '../../config/mysqlDb';
+import validateDb from '../../utils/validateDB';
+import { Repository } from '../../repositories/repository';
 
 const newsRouter: Router = express.Router();
 
-newsRouter.get("/", (_req: Request, res: Response) => {
-  return res.json("news");
+newsRouter.get('/', async (_req: Request, res: Response) => {
+  try {
+    const repository = new Repository<INewsWithoutContent>('news', ['id', 'title', 'image', 'publication_date']);
+    const news = await repository.getAll();
+    return res.json(news);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 });
+
+newsRouter.post(
+  '/',
+  imagesUpload.single('image'),
+  async (req: Request, res: Response, next) => {
+    const news: INews = req.body;
+
+    const correctNewsData: INews = {
+      ...news,
+      title: news.title,
+      content: news.content || null,
+      image: req.file ? `images/${req.file.filename}` : null,
+    };
+
+    try {
+      const isValidate = validateDb.validateCreateData(correctNewsData, [
+        'title',
+        'content',
+      ]);
+      if (!isValidate)
+        return res.status(400).json({ error: 'Title or content is empty' });
+
+      const connection: Connection = await mysqlDb.getConnection();
+      const [result] = await connection.query(
+        `INSERT INTO news (title, content, image) VALUES (?, ?, ?)`,
+        [correctNewsData.title, correctNewsData.content, correctNewsData.image],
+      );
+
+      const resultHeader: ResultSetHeader = result as ResultSetHeader;
+
+      const [insertedNews] = await connection.query(
+        `SELECT * FROM news WHERE id = ?`,
+        [resultHeader.insertId],
+      );
+
+      const createdNews = insertedNews as INews[];
+
+      res.status(201).json(...createdNews);
+    } catch (error) {
+      next(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+);
 
 export default newsRouter;
